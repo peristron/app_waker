0edc940a1fe72f3f6dff9d2c4f678f0fd6d9d3d8
omar akhtar <oakhtar@d2l.com>
2026-01-01 17:33:26 -0500

    Update wake_apps.py

--- wake_apps.py
+++ wake_apps.py
@@ -21,7 +21,8 @@ app_urls = [
     "https://wordcloudandsentimentanalyzer2.streamlit.app",
     "https://datasetexpl0rerupgraded.streamlit.app",
     "https://w0rdcl0udharvesterv4.streamlit.app",
-    "https://signalfoundry.streamlit.app"
+    "https://signalfoundry.streamlit.app",
+    "https://lineageanddependencieschecker.streamlit.app"
 ]
 
 def get_driver():
